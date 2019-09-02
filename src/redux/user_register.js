6ff55caf87c51a0e96b9dd5243fd5febcbfc2ef2
Brandon Caig <bcraig2612@gmail.com>
2019-09-02 09:44:43 -0700

    .

--- src/redux/user_register.js
+++ src/redux/user_register.js
@@ -7,13 +7,6 @@ const initialState = {
 const SET_LOGIN_USER = 'SET_LOGIN_USER'
 const SET_REGISTER_USER = 'SET_REGISTER_USER'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
-// const FIRST_NAME = 'FIRST_NAME'
-// const LAST_NAME = 'LAST_NAME'
-// const USER_NAME = 'USER_NAME'
-// const EMAIL = 'EMAIL'
-// const PASSWORD = 'PASSWORD'
-// const SEX = 'SEX'
-// const PROFILE_PICTURE = 'PROFILE_PICTURE'
 
 export default function reducer(state = initialState, action) {
     const { type, payload } = action;
@@ -22,21 +15,7 @@ export default function reducer(state = initialState, action) {
         case SET_USER_PROFILE:
         case SET_LOGIN_USER:
             console.log('this is the user who is in', payload);
-            return { ...state, user:payload };
-        // case FIRST_NAME:
-        //     return { ...state, first_name: payload };
-        // case LAST_NAME:
-        //     return { ...state, last_name: payload };
-        // case USER_NAME:
-        //     return { ...state, user_name: payload };
-        // case EMAIL:
-        //     return { ...state, email: payload };
-        // case PASSWORD:
-        //     return { ...state, password: payload };
-        // case SEX:
-        //     return { ...state, sex: payload };
-        // case PROFILE_PICTURE:
-        //     return { ...state, sex: payload };
+            return { ...state, user: payload };
         default:
             return state;
     }
@@ -62,48 +41,3 @@ export function setUserProfile(profile) {
         payload: profile
     }
 }
-// export function insertUserName(username) {
-//     return {
-//         type: USER_NAME,
-//         payload: username
-//     }
-// }
-
-// export function insertFirstName(first) {
-//     return {
-//         type: FIRST_NAME,
-//         payload: first
-//     }
-// }
-
-// export function insertLastName(last) {
-//     return {
-//         type: LAST_NAME,
-//         payload: last
-//     }
-// }
-
-// export function insertEmail(email) {
-//     return {
-//         type: EMAIL,
-//         payload: email
-//     }
-// }
-// export function insertPassword(password) {
-//     return {
-//         type: PASSWORD,
-//         payload: password
-//     }
-// }
-// export function insertSex(sex) {
-//     return {
-//         type: SEX,
-//         payload: sex
-//     }
-// }
-// export function insertProfilePicture(url) {
-//     return {
-//         type: PROFILE_PICTURE,
-//         payload: url
-//     }
-// }
